78b00ef2ed2889a502200a4cdfe70cb626c8e44a
marccodera <marc.codera@gmail.com>
2018-02-20 18:08:20 +0100

    created Database

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect, jsonify, url_for, f
 # IMPORTS FOR DATABASE CONNECTION AND OPERATIONS
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem, User
+# from database_setup import Base, Restaurant, MenuItem, User
 
 # [Anti Forge] Imports needed
 from flask import session as login_session
@@ -24,34 +24,46 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/catalog/')
 def showCatalog():
+    response = 'show catalog'
+    return response
 
 
 # Show Items in Category
-@app.route('/catalog/<string:category>/items/')
+@app.route('/catalog/<category>/items/')
 def showCategoryItems(category):
-
-
-# Show Item information
-@app.route('/catalog/<string:category>/<string:item>/')
-def showItemInfor(category, item):
+    response = 'show category items in: %s' % category 
+    return response
 
 
 # Add Item in Catalog
 @app.route('/catalog/new/', methods=['GET','POST'])
 def newItem():
+    response = 'new item'
+    return response
 
 
 # Edit Item in catalog
-@app.route('/catalog/<string:item>/edit', methods=['GET','POST'])
+@app.route('/catalog/<item>/edit', methods=['GET','POST'])
 def editItem(item):
-
+    response = 'edit item'
+    return response
 
 # Delete Item in catalog
-@app.route('/catalog/<string:item>/delete', methods=['GET','POST'])
+@app.route('/catalog/<item>/delete', methods=['GET','POST'])
 def deleteItem(item):
+    response = 'delete item: '
+    return response
+
+
+# Show Item information
+@app.route('/catalog/<category>/<string:item>/')
+def showItemInfor(category, item):
+    response = 'show item information'
+    return response
+
 
 
 if __name__ == '__main__':
-  app.secret_key = 'super_secret_key'
-  app.debug = True
-  app.run(host = '0.0.0.0', port = 5000)
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 8000)
