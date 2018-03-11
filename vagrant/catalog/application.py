840215ab85083278e15ac8af6095f0749cb1c1e9
marccodera <marc.codera@gmail.com>
2018-03-11 16:12:14 +0100

    Added Google Authentication, and login.html, now user access is needed to review

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -20,6 +20,13 @@ import requests
 
 app = Flask(__name__)
 
+# Flow from client secrets stores client information in JSON file, this gets the JSON file
+CLIENT_ID = json.loads(
+    # client_secrets.json is from google dev website! 
+    # It's important to use the same APPname here as in Google website
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Catalog Application"
+
 #Connect to Database and create database session
 engine = create_engine('sqlite:///catalog.db')
 # Bind the engine to the metadata of the Base class so that the
@@ -36,6 +43,181 @@ DBSession = sessionmaker(bind=engine)
 # session.rollback()
 session = DBSession()
 
+
+# [User Authentication] - Start
+# [Anti Forge] - Code needed to generate state token
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
+
+# [Google Oauth] - Server Side function for google connect
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token the client sent to the server matches the one server sent to the client
+    if request.args.get('state') != login_session['state']:
+        # If they don't match it replies ith an error indicating there's a missmatch between the tokens
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        # If there's no error it can proceed
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain one time authorization code from the server
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object generating one time code
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check to see if user is already logged in
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    # see if user exists. If it doesn't make a new one
+    user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+
+# DISCONNECT - Revoke a current user's token and reset their login_session
+#This part is used to disconnect the session
+@app.route('/gdisconnect')
+def gdisconnect():
+      # Only disconnect a connected user.
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    #print 'In gdisconnect access token is %s', access_token
+    #print 'User name is: '
+    #print login_session['username']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    
+    #print 'result is '
+    #print result
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+         # For whatever reason, the given token was invalid.
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+# User Helper Functions
+
+
+
+#Returns User ID if exists on the database based on email address
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email = email).one()
+        return user.id
+    except:
+        return None
+
+
+#Getting user object with all the info from the database
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id = user_id).one()
+    return user
+
+
+#This class creates a user on the database related to login_session parameter
+def createUser(login_session):
+    newUser = User(name = login_session['username'], email = 
+      login_session['email'], picture = login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email = login_session
+      ['email']).one()
+    return user.id
+
+
+# [Page Building] - Start
 # Show Catalog
 @app.route('/')
 @app.route('/catalog/')
@@ -48,6 +230,7 @@ def showCatalog():
     #     return render_template('publiccatalog.html', categories = categories)
     return render_template('catalog.html', categories = categories,
                            items = items)
+
 
 # Show Items in Category
 @app.route('/catalog/<category_name>/items/')
@@ -120,6 +303,7 @@ def editItem(category, item):
     else:
         return render_template('edititem.html', 
           category = itemInfo.category.name, item = itemInfo)
+
 
 # Delete Item in catalog
 @app.route('/catalog/<category>/<item>/delete', methods=['GET','POST'])
